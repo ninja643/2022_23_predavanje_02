a4e8e54ef825c2075ebff0155b8d61952e61105c
Marko Milosevic <markom@niri.rs>
2022-10-25 00:18:09 +0200

    Initial commit

---
+++ src/rs/ac/ni/pmf/oop2/ObjectEquivalence.java
@@ -0,0 +1,12 @@
+package rs.ac.ni.pmf.oop2;
+
+public class ObjectEquivalence
+{
+	public static void main(String[] args)
+	{
+		final Integer a = new Integer(42);
+		final Integer b = new Integer(42);
+		System.out.println(a == b);
+		System.out.println(a != b);
+	}
+}
